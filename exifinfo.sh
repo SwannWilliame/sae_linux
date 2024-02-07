5d50fe4973c61fd59bcce97911b447000824b91b
SwannWilliame <swann.williame@gmail.com>
2024-02-07 16:56:45 +0100

    correct exifinfo

--- exifinfo.sh
+++ exifinfo.sh
@@ -33,55 +33,31 @@ then
     do
         case $i in 
             "rx")
-                echo $(exif $filename | sed '5q;d' | cut -d "|" -f2)
+                echo $(exif $filename | grep "^R'esolution X" | cut -d "|" -f2)
                 ;;
             "ry")
-                echo $(exif $filename | sed '6q;d' | cut -d "|" -f2)
+                echo $(exif $filename | grep "^R'esolution Y" | cut -d "|" -f2)
                 ;;
             "runit")
-                echo $(exif $filename | sed '7q;d' | cut -d "|" -f2)
-                ;;
-            "logiciel")
-                echo $(exif $filename | sed '8q;d' | cut -d "|" -f2)
+                echo $(exif $filename | grep "^Unit" | cut -d "|" -f2)
                 ;;
             "date")
-                echo $(exif $filename | sed '9q;d' | cut -d "|" -f2)
+                echo $(exif $filename | grep "^Date et heure " | cut -d "|" -f2)
                 ;;
             "largeur")
-                echo $(exif $filename | sed '10q;d' | cut -d "|" -f2)
+                echo $(exif $filename | grep "^Largeur" | cut -d "|" -f2)
                 ;;
             "longueur")
-                echo $(exif $filename | sed '11q;d' | cut -d "|" -f2)
-                ;;
-            "bit")
-                echo $(exif $filename | sed '12q;d' | cut -d "|" -f2)
+                echo $(exif $filename | grep "^Longueur" | cut -d "|" -f2)
                 ;;
             "compression")
-                echo $(exif $filename | sed '13q;d' | cut -d "|" -f2)
-                ;;
-            "interpret")
-                echo $(exif $filename | sed '14q;d' | cut -d "|" -f2)
+                echo $(exif $filename | grep "^Compression" | cut -d "|" -f2)
                 ;;
             "echantillon")
-                echo $(exif $filename | sed '15q;d' | cut -d "|" -f2)
+                echo $(exif $filename | grep "^'Echantillons" | cut -d "|" -f2)
                 ;;
-            "espcouleur")
-                echo $(exif $filename | sed '19q;d' | cut -d "|" -f2)
-                ;;
-            "exifv")
-                echo $(exif $filename | sed '20q;d' | cut -d "|" -f2)
-                ;;
-            "flashv")
-                echo $(exif $filename | sed '21q;d' | cut -d "|" -f2)
-                ;;
-            "lat")
-                echo $(exif $filename | sed '22q;d' | cut -d "|" -f2)
-                ;;
-            "long")
-                echo $(exif $filename | sed '23q;d' | cut -d "|" -f2)
-                ;;
-            "altitude")
-                echo $(exif $filename | sed '24q;d' | cut -d "|" -f2)
+            "desc")
+                echo $(exif $filename | grep "^Description" | cut -d "|" -f2)
                 ;;
             *)
                 echo "le $i paramètre n'existe pas"
