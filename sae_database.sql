d9e6f4305757e4ddc645c3acdf95cde26846b17a
SwannWilliame <swann.williame@gmail.com>
2024-02-08 10:21:56 +0100

    changement

--- sae_database.sql
+++ sae_database.sql
@@ -10,9 +10,8 @@ CREATE TABLE IF NOT EXISTS galaxie(
     PRIMARY KEY(id)
 ) ;
 
-
-DROP TABLE IF EXISTS systeme_solaire;
-CREATE TABLE IF NOT EXISTS systeme_solaire(
+DROP TABLE IF EXISTS constellation;
+CREATE TABLE IF NOT EXISTS constellation(
     id SERIAL,
     nom VARCHAR(255) NOT NULL,
     PRIMARY KEY(id)
@@ -58,7 +57,7 @@ CREATE TABLE IF NOT EXISTS image(
     categorie_id BIGINT CHECK (categorie_id > 0) NOT NULL,
     unite_res_id BIGINT CHECK (unite_res_id > 0) NOT NULL,
     type_compression_id BIGINT CHECK (type_compression_id > 0),
-    systeme_solaire_id BIGINT CHECK (systeme_solaire_id > 0),
+    constellation_id BIGINT CHECK (constellation_id > 0),
     galaxie_id  BIGINT CHECK (galaxie_id > 0) NOT NULL,
     PRIMARY KEY(id)
 ) ;
@@ -66,5 +65,5 @@ CREATE TABLE IF NOT EXISTS image(
 ALTER TABLE image ADD CONSTRAINT fk_image_categorie_id FOREIGN KEY(categorie_id) REFERENCES categorie (id) ON DELETE CASCADE ON UPDATE CASCADE;
 ALTER TABLE image ADD CONSTRAINT fk_image_unite_res_id FOREIGN KEY(unite_res_id) REFERENCES unite_res (id) ON DELETE CASCADE ON UPDATE CASCADE;
 ALTER TABLE image ADD CONSTRAINT fk_image_type_compression_id FOREIGN KEY(type_compression_id) REFERENCES type_compression (id) ON DELETE CASCADE ON UPDATE CASCADE;
-ALTER TABLE image ADD CONSTRAINT fk_image_systeme_solaire_id FOREIGN KEY(systeme_solaire_id) REFERENCES systeme_solaire (id) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE image ADD CONSTRAINT fk_image_constellation_id FOREIGN KEY(constellation_id) REFERENCES constellation (id) ON DELETE CASCADE ON UPDATE CASCADE;
 ALTER TABLE image ADD CONSTRAINT fk_image_galaxie_id FOREIGN KEY(galaxie_id) REFERENCES galaxie (id) ON DELETE CASCADE ON UPDATE CASCADE;
