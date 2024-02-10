21abe68592bede2ad83ab89759f65da54f89401f
Swann Williame <swann.williame@gmail.com>
2024-02-10 18:13:04 +0100

    static data

---
+++ static_data_insert.sql
@@ -0,0 +1,14 @@
+USE sae_database;
+INSERT INTO galaxie (id, nom) VALUES(1, 'Voie lactee'),
+(2, 'Cosmic Rose'),
+(3, 'Galaxie du Moulinet');
+INSERT INTO constellation (id, nom) VALUES(1, 'Carène'),
+(2, 'Andromède'),
+(3, 'Gemaux'),
+(4, 'Grande Ours'),
+(5, 'Ursa Major'),
+(6, 'Céphée'),
+(7, 'Cygnus'),
+(8, 'Orion'),
+(9, 'Sagittaire'),
+(10, 'Serpent');
