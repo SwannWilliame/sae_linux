e280fa466293e3ea3749cf02024346426e94f0ad
Swann Williame <swann.williame@gmail.com>
2024-02-10 16:22:59 +0100

    dzfszedfgsdgf

--- sql_exe.sh
+++ sql_exe.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+
+
 if [ $# -lt 1 ]
+
 then
+
     echo "aucun paramètre n'a été donné"
+
     exit 1;
+
 fi
 
-su -c "psql -d sae_database -c \"$*\"" postgres
+
+
+sudo mysql sae_database -e "$*";
