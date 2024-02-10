ff9ab5446693e81a7bd99cff61cf5cd108db45c5
SwannWilliame <swann.williame@gmail.com>
2024-02-10 15:56:10 +0100

    correct mysql script

--- sae_database.sql
+++ sae_database.sql
@@ -1,21 +1,21 @@
 DROP DATABASE IF EXISTS sae_database;
-CREATE DATABASE sae_database;
+CREATE DATABASE IF NOT EXISTS sae_database CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_general_ci';
 
-\c sae_database;
+USE sae_database;
 
 DROP TABLE IF EXISTS galaxie;
 CREATE TABLE IF NOT EXISTS galaxie(
     id SERIAL,
     nom VARCHAR(255) NOT NULL,
     PRIMARY KEY(id)
-) ;
+) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_general_ci';
 
 DROP TABLE IF EXISTS constellation;
 CREATE TABLE IF NOT EXISTS constellation(
     id SERIAL,
     nom VARCHAR(255) NOT NULL,
     PRIMARY KEY(id)
-) ;
+) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_general_ci';
 
 
 DROP TABLE IF EXISTS type_compression;
@@ -23,7 +23,7 @@ CREATE TABLE IF NOT EXISTS type_compression(
     id SERIAL,
     nom VARCHAR(255) NOT NULL,
     PRIMARY KEY(id)
-) ;
+) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_general_ci';
 
 
 DROP TABLE IF EXISTS unite_res;
@@ -31,7 +31,7 @@ CREATE TABLE IF NOT EXISTS unite_res(
     id SERIAL,
     nom VARCHAR(255) NOT NULL,
     PRIMARY KEY(id)
-) ;
+) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_general_ci';
 
 
 DROP TABLE IF EXISTS categorie;
@@ -39,28 +39,28 @@ CREATE TABLE IF NOT EXISTS categorie(
     id SERIAL,
     nom VARCHAR(255) NOT NULL,
     PRIMARY KEY(id)
-) ;
+) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_general_ci';
 
 
 DROP TABLE IF EXISTS image;
 CREATE TABLE IF NOT EXISTS image(
     id SERIAL,
     nom VARCHAR(255) NOT NULL,
-    description_image TEXT,
+    description_image LONGTEXT,
     res_x INT NOT NULL,
     res_y INT NOT NULL,
-    date_creation TIMESTAMP(0),
+    date_creation DATETIME(0),
     largeur INT,
     longueur INT,
     echantillon_par_pixel INT,
-    image_data BYTEA NOT NULL,
+    image_data LONGBLOB NOT NULL,
     categorie_id BIGINT CHECK (categorie_id > 0) NOT NULL,
     unite_res_id BIGINT CHECK (unite_res_id > 0) NOT NULL,
     type_compression_id BIGINT CHECK (type_compression_id > 0),
     constellation_id BIGINT CHECK (constellation_id > 0),
     galaxie_id  BIGINT CHECK (galaxie_id > 0) NOT NULL,
     PRIMARY KEY(id)
-) ;
+) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_general_ci';
 
 ALTER TABLE image ADD CONSTRAINT fk_image_categorie_id FOREIGN KEY(categorie_id) REFERENCES categorie (id) ON DELETE CASCADE ON UPDATE CASCADE;
 ALTER TABLE image ADD CONSTRAINT fk_image_unite_res_id FOREIGN KEY(unite_res_id) REFERENCES unite_res (id) ON DELETE CASCADE ON UPDATE CASCADE;
